faeb3d19dcc45271061ad05416a15f47031f7583
profrbazurph <profrbazurph@gmail.com>
2023-02-12 08:38:37 +0800

    Methods

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import { Link, NavLink } from "react-router-dom"; 
-import Logo from "../assets/logo.png"
-import "./Header.css";
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../assets/logo.png';
+import { useCart } from '../context/CartContext';
+import './Header.css';
 
 export const Header = () => {
+  const { cartList } = useCart();
+
   return (
     <header>
       <Link to="/" className="logo">
@@ -10,12 +13,16 @@ export const Header = () => {
         <span>Shopping Cart</span>
       </Link>
       <nav className="navigation">
-        <NavLink to="/" className="link" end>Home</NavLink>
-        <NavLink to="/cart" className="link">Cart</NavLink>
+        <NavLink to="/" className="link" end>
+          Home
+        </NavLink>
+        <NavLink to="/cart" className="link">
+          Cart
+        </NavLink>
       </nav>
       <Link to="/cart" className="items">
-        <span>Cart: 2</span>
+        <span>Cart: {cartList.length}</span>
       </Link>
     </header>
-  )
-}
+  );
+};
